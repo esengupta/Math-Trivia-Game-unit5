7523f3c62626ae6c43f7b2e6e04a555e7ba8b423
enakshi sengupta <enakshi.kundu@gmail.com>
2018-09-04 17:43:46 -0400

    updated

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,13 @@
 
 var questions = [{
-  ques: "1. If there is a basket of 13 apples and you take three of them, how many apples do you have?",
+  ques: "If there is a basket of 13 apples and you take three of them, how many apples do you have?",
   ans: ["10", "1", "16", "3"],
   name: "apple",
   correct: "10",
   divClass: ".apple"
 },
 {
-  ques: "2. How many 2-cent stamps does it take to make a dozen?",
+  ques: "How many 2-cent stamps does it take to make a dozen?",
   ans: ["0", "6", "12", "3"],
   name: "stamp",
   correct: "6",
@@ -15,28 +15,28 @@ var questions = [{
 },
 
 {
-  ques: "3. How many gallons are in 20 quarts?",
+  ques: "How many gallons are in 20 quarts?",
   ans: ["10", "5", "2.5", "4."],
   name: "gallon",
   correct: "5",
   divClass: ".gallon"
 },
 {
-  ques: "4. What is 84 inches in feet and inches?",
+  ques: "What is 84 inches in feet and inches?",
   ans: ["6 Feet 8 Inches", "7 Feet 0 Inches", "10 Feet 8 Inches", "8 Feet 0 Inches"],
   name: "feet",
   correct: "7 Feet 0 Inches",
   divClass: ".feet"
 },
 {
-  ques: "5. If you divide 14 by 1/2, what do you get?",
+  ques: "If you divide 14 by 1/2, what do you get?",
   ans: ["7", "0.50", "2", "28"],
   name: "divide",
   correct: "7",
   divClass: ".divide"
 },
 {
-  ques: "6. Within the same day, how much time will pass between 2:15 PM and 5:15 PM?",
+  ques: "Within the same day, how much time will pass between 2:15 PM and 5:15 PM?",
   ans: ["3 Hours 15 Minutes", "2 Hours 45 Minutes", "3 Hours", "2 Hours 15 Minutes"],
   name: "hours",
   correct: "3 Hours",
@@ -47,28 +47,33 @@ var questions = [{
 
 var labels = ["first", "second", "third", "forth"];
 
-// click to start then display quesions
-var startGame = $("#start-btn").on('click', function() {
-$(this).parent().hide();
-$('.container').show();
-countdown(40);
-questionDisplay();
-});
 
 // function for displaying questions
-var questionDisplay = function() {
-$(".questions").empty();
+function questionDisplay () {
+    // $(".questions :not('#sub-but')").empty();
 // loops through the 6 questions 
-for (var j = 0; j < questions.length; j++) {
-$('.questions').append('<div class="' + questions[j].name + '"></div>');
-$(questions[j].divClass).append('<div class ="ques-title">' + questions[j].ques + '</div>');
-// loops through answers for each radio button
-for (var i = 0; i <= 3; i++) {
-  $(questions[j].divClass).append('<input type="radio"  name="' + questions[j].name + '" value="' + questions[j].ans[i] + '"/><label for="' + labels[i] + '">' + questions[j].ans[i] + '</label>');
+  for (var j = 0; j < questions.length; j++) {
+    // $('.questions').prepend('<div class="' + questions[j].name + '"></div>');
+    var question = $('<div>');
+    question.addClass( questions[j].name)
+    question.append('<div class ="ques-title">' + questions[j].ques + '</div>');
+    // loops through answers for each radio button
+    for (var i = 0; i <= 3; i++) {
+      question.append('<input type="radio"  name="' + questions[j].name + '" value="' + questions[j].ans[i] + '"/><label for="' + labels[i] + '">' + questions[j].ans[i] + '</label>');
+    }
+    $('.questions').prepend(question);
+    $('.questions').prepend('<hr />');
+  }
 }
-$('.questions').append('<hr />');
-}
-}
+
+
+// click to start then display quesions
+$("#start-btn").on('click', function() {
+  $(this).parent().hide();
+  $('.container').show();
+  countdown(40);
+  questionDisplay();
+});
 
 
 // function for countdown timer
